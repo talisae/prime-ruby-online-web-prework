b0822537234867013ab1d1198ad5ea1061073dfd
Talisa Erazo <talisaerazo@gmail.com>
2018-10-16 14:47:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
-def prime?(num)
-  (2...Math.sqrt(num)).all? { |i| num % i != 0 }
+def prime?(n)
+  return false if n < 2
+  return true if n == 3 || n == 2
+    if (2...n-1).any?{|i| n % i == 0}
+      false
+    else
+      true
+    end
 end
